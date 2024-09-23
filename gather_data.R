7baa2e9483490164f77a61da760b6e297ff07feb
QuinnAsena <qase352@aucklanduni.ac.nz>
2024-09-23 09:28:49 -0500

    .

--- gather_data.R
+++ gather_data.R
@@ -27,7 +27,7 @@ if(using_googlesheets){
 } else {
   
   # Want to go oldschool with just a csv?
-  position_data <- read_csv("csvs/positions.csv")
+  position_data <- read_csv("csvs/positions2pg-temp.csv")
   skills        <- read_csv("csvs/language_skills.csv")
   text_blocks   <- read_csv("csvs/text_blocks.csv")
   contact_info  <- read_csv("csvs/contact_info.csv", skip = 1)
